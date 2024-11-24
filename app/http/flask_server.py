b14027dc50e288c2885904c16c7fdd9127d4031c
Rafael Cecchin <contatorafaelcecchin@gmail.com>
2024-11-24 18:29:17 -0300

    feat: detect my face

--- app/http/flask_server.py
+++ app/http/flask_server.py
@@ -11,5 +11,5 @@ def index():
     return render_template('index.html', ws_port=os.getenv('WEBSOCKET_PORT', 8765))
 
 if __name__ == '__main__':
-    port = int(os.getenv('WEBSITE_PORT', 5000))
+    port = int(os.getenv('WEBSITE_PORT', 80))
     app.run(host='0.0.0.0', port=port)
--- app/http/websocket_server.py
+++ app/http/websocket_server.py
@@ -5,14 +5,24 @@ import websockets
 
 async def video_stream(websocket):
     cap = cv2.VideoCapture(os.getenv('SOURCEVIDEO'))
-    
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
     try:
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
 
-            ret, encimg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 1])
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+            
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(frame_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+            
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            ret, encimg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
 
             try:
                 await websocket.send(encimg.tobytes())
